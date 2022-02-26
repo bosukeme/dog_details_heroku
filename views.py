2c4a781e28db325ff944dc6f1fc1af2a65cf164e
bloverseukeme <ukeme.wilson@bloverse.com>
2022-02-26 15:29:49 +0100

    initial files

--- views.py
+++ views.py
@@ -1,27 +1,27 @@
-from flask import Blueprint, jsonify
-from flask_restful import Api, Resource
-from scopezero.resources.dog_resources import DogDetails, DogDetailsCSV
-from scopezero.resources.dog_resources import (
-            GenerateLeatherCollar, GenerateLeatherLeach, GeneratePolyester, CleanUpNumber)
+# from flask import Blueprint, jsonify
+# from flask_restful import Api, Resource
+# from scopezero.resources.dog_resources import DogDetails, DogDetailsCSV
+# from scopezero.resources.dog_resources import (
+#             GenerateLeatherCollar, GenerateLeatherLeach, GeneratePolyester, CleanUpNumber)
 
 
-class Home(Resource):
-    def get(self):
-        return jsonify({
-            "message": "Welcome to the DOG Details  pipeline!"
-        })
+# class Home(Resource):
+#     def get(self):
+#         return jsonify({
+#             "message": "Welcome to the DOG Details  pipeline!"
+#         })
 
 
-dog_details = Blueprint(
-    "dog_details", __name__
-)
+# dog_details = Blueprint(
+#     "dog_details", __name__
+# )
 
-api = Api(dog_details)
+# api = Api(dog_details)
 
-api.add_resource(Home, '/')
-api.add_resource(DogDetails, "/dog_details")
-api.add_resource(DogDetailsCSV, "/dog_details_csv")
-api.add_resource(GenerateLeatherCollar, "/generate_collar")
-api.add_resource(GenerateLeatherLeach, "/generate_leash")
-api.add_resource(GeneratePolyester, "/generate_polyester")
-api.add_resource(CleanUpNumber, "/clean_up_number")
+# api.add_resource(Home, '/')
+# api.add_resource(DogDetails, "/dog_details")
+# api.add_resource(DogDetailsCSV, "/dog_details_csv")
+# api.add_resource(GenerateLeatherCollar, "/generate_collar")
+# api.add_resource(GenerateLeatherLeach, "/generate_leash")
+# api.add_resource(GeneratePolyester, "/generate_polyester")
+# api.add_resource(CleanUpNumber, "/clean_up_number")
